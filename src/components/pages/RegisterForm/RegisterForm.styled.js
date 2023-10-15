42a6cf786dccad81ae90bea3cc8c74783255cc70
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2023-10-15 22:06:14 +0300

    Fix bag formik

--- src/components/pages/RegisterForm/RegisterForm.styled.js
+++ src/components/pages/RegisterForm/RegisterForm.styled.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Field } from "formik";
 
 export const RegistrFormStyle = styled.div`
 
